f671bb956aab1b3ef7fcbb776e4cd9ce589258b2
ziadmo5amed <161350029+ziadmo5amed@users.noreply.github.com>
2025-03-30 17:27:01 +0200

    frist

---
+++ main.js
@@ -0,0 +1,49 @@
+// string method
+//length 1
+//index 0
+let name ="i love javascript";
+                               
+
+console.log(name);
+
+console.log(name.length);
+console.log(name[3]);
+
+console.log(name.repeat(10));
+
+// index of = start (element , start)
+//last index =of end (element , start)
+
+console.log(name.indexOf("i" , 15));
+
+console.log(name.lastIndexOf("i",13));
+
+// slice = start , end
+console.log(name.slice(5));
+
+//split = start , end
+console.log(name.split("i"));
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+   
+ 
+ 
+
+
+ 
+ 
+ 
+
+
